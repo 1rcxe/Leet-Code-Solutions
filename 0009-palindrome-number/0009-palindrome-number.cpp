769ab6f1a790d39cfe793b6b5f922f015a299555
Hichem Hanewi <117721381+1rcxe@users.noreply.github.com>
2022-11-15 22:19:12 +0100

    Time: 26 ms (60.22%), Space: 5.9 MB (32.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long int num=x ;
+        long long int rev =0;
+        if (num < 0){
+            return false;
+        }
+        while (num>0){
+            rev = (rev*10)+num%10;
+            num=num/10;
+        }
+        return x == rev ;
+        
+                 
+        
+    }
+};
